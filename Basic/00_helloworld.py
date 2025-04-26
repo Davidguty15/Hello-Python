2e8273fbb54ac0663bda534d5f3a9ef218da2a21
David Gutierrez <david15.org@gmail.com>
2025-04-25 21:34:18 -0500

    Cambio uno

--- Basic/00_helloworld.py
+++ Basic/00_helloworld.py
@@ -27,3 +27,6 @@ print(type(1.5))  # Tipo 'float'
 print(type(3 + 1j))  # Tipo 'complex'
 print(type(True))  # Tipo 'bool'
 print(type(print("Mi cadena de texto")))  # Tipo 'NoneType'
+
+
+##Cambio uno
